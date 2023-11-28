ae643c61bfce1223a26a73978455c34884ac48dd
Baptisteismd <baptiste.ismedon@utt.fr>
2023-11-28 18:26:30 +0100

    spec3 définitive

--- SPEC3 GL02/hello.js
+++ SPEC3 GL02/hello.js
@@ -19,11 +19,12 @@ function exam(tableauDeDonnees) {
 
 // Appeler la fonction exam avec le tableau tableauDeDonnees
 exam(tableauDeDonnees);
-//stocker l'exam dans un fichier
-let data = tableauDeDonnees.join('\n')
 
+let data = tableauDeDonnees.join('\n')
+//trouver le chemin du dossier
 let dossier = 'C:\\Users\\bapti\\Documents\\20232024\\GL02\\projet\\ProjetGIT\\SUJETB_AouRien\\SPEC3 GL02\\exam crée'
 let examen = path.join(dossier,'exam.gif')
+//stocker l'exam dans un fichier
 fs.writeFile(examen, data, (err) => {
     if (err) throw err;
     console.log('Les données ont été écrites dans le fichier avec succès.');
