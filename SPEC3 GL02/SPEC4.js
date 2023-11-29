89c062f903109977df6b5a2a24661f8d863baf54
Baptisteismd <baptiste.ismedon@utt.fr>
2023-11-29 15:41:06 +0100

    spec3 définitive

---
+++ SPEC3 GL02/SPEC4.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const path = require('path');
+const readline = require('readline');
+
+// Lecture du fichier JSON
+const jsonFilePath = path.join(__dirname, 'data.json');
+const jsonData = fs.readFileSync(jsonFilePath, 'utf-8');
+let tableauDeDonnees = JSON.parse(jsonData);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+function demanderUneReponse(question) {
+    return new Promise((resolve) => {
+        rl.question(question, (reponseUtilisateur) => {
+            resolve(reponseUtilisateur);
+        });
+    });
+}
+
+async function simuleexam(tableauDeDonnees) {
+    for (let i = 0; i < tableauDeDonnees.length; i++) {
+        // je crée des questions propre
+        tableauDeDonnees[i].questionText = tableauDeDonnees[i].questionText.replace(/[{]/g, "")
+        tableauDeDonnees[i].questionText = tableauDeDonnees[i].questionText.replace(/ *\([^)]*\) */g, "");
+        tableauDeDonnees[i].questionText = tableauDeDonnees[i].questionText.replace(/ *\<[^>]*\> */g, "");
+        tableauDeDonnees[i].questionText = tableauDeDonnees[i].questionText.replace(/ *\[[^]*\] */g, "");
+        console.log(tableauDeDonnees[i].questionText);
+        tableauDeDonnees[i].reponses.forEach((reponse, index) => {
+            if (reponse.includes("~")) {
+                console.log(`${index + 1}. ${reponse.replace("~", "")}`);
+            }
+        });
+        let reponse = await demanderUneReponse("\nVeuillez entrer le numéro de votre réponse :");
+        console.log(`Vous avez choisi la réponse numéro: ${reponse}`);
+    }
+    rl.close();
+}
+
+simuleexam(tableauDeDonnees);
