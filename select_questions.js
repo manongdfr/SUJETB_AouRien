ea236f2cd8de98e0fe4654d62acf7852585290a8
Acrosiac84 <romain.rudent84@gmail.com>
2023-12-03 16:44:48 +0100

    Modif de recherche et extraction des données 2.js et de la fct de selection des questions version final tout marche

--- select_questions.js
+++ select_questions.js
@@ -1,5 +1,4 @@
 
-
 function select_questions() {
     const {
         prompt,
@@ -9,22 +8,33 @@ function select_questions() {
     let choix2 = "0";
     var tab_values = [];
     var tab_select = [];
+    var can_create = false;
     while (choix2 !== "n") {
-        choix2 = prompt("Effectuer une nouvelle recherche (y/n) : ","n");
+        if (tab_select.length >= 20) {
+            console.log("Nombre max de question ajoutées");
+            choix2 = "n";
+        } else {
+            choix2 = prompt("Effectuer une nouvelle recherche (y/n) : ", "n");
             if (choix2.toLowerCase() === "y") {
                 var tag = prompt("Saisir le tag de la recherche : ")
-                var  text_question = prompt("Saisir la chaine de caractère de la question : ")
+                var text_question = prompt("Saisir la chaine de caractère de la question : ")
                 if (tag || text_question) {
                     tab_values = recherche(text_question, tag);
                 }
                 console.log("Voici les resultat de votre recherche");
                 console.table(tab_values);
                 let choix = "0";
-                while (choix !== "stop" ) {
-                    choix = "stop";
-                    choix = prompt("Selectionner l'index de votre question a selectionner ( stop pour arreter la selection) : ","stop")
+                while (choix !== "stop") {
+                    if (tab_select.length>=20){
+                        console.log("Nombre max de question ajoutées");
+                        choix = "stop";
+                        choix2 = "n";
+                    }
+                    else {
+                        choix = "stop";
+                        choix = prompt("Selectionner l'index de votre question a séléctionner ( stop pour arreter la selection) : ", "stop")
                         if (choix.toLowerCase() !== "stop") {
-                            if (choix <= tab_values.length && choix >= 0 ) {
+                            if (choix <= tab_values.length && choix >= 0) {
                                 var entry = {
                                     tag: tab_values[choix]["tag"],
                                     questionText: tab_values[choix]["questionText"],
@@ -33,12 +43,30 @@ function select_questions() {
                                 tab_select.push(entry);
                                 console.log("Votre selection actuelle : ")
                                 console.table(tab_select);
+                                console.log(`il vous reste encore ${20 - tab_select.length} places `)
+                                if (tab_select.length < 15) {
+                                    console.log(`Vous devais rajouter encore ${15 - tab_select.length} au minimum pour former un exam `)
+                                } else {
+                                    console.log("Vous avez ajouter suffisament de question pour cree un exam")
+                                    can_create = true;
+                                }
                             } else {
                                 console.log("Veuillez choisir un index valide")
                             }
                         }
+                    }
                 }
             }
+        }
+    }
+    if (can_create){
+        return tab_select
+    }
+    else {
+        var choix3 = prompt("Vous n'avez pas remplie les conditions necesaire pour cree l'exam voulez vous recommencer (y/n)","n")
+        if (choix3.toLowerCase() === "y"){
+            select_questions()
+        }
     }
 }
 select_questions()
